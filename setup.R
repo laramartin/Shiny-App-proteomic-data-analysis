809419791fde125ec4d17db37b9b345a4554fa8b
Lara Martin <larikimc@gmail.com>
2015-06-09 18:37:31 +0200

    adding choice 4 to ui

--- setup.R
+++ setup.R
@@ -1,13 +1,19 @@
-# Setup for MS-app
 
-listPackages <- c("BiocInstaller", "shiny", "lattice")
+###################################################
+#   LARA MARTIN - proyecto final de posgrado      #
+#   setup.R                                       #
+###################################################
 
+# for install RforProteomics from source
 if(!require("BiocInstaller", quietly = T))  
   install.packages("BiocInstaller")
 require("BiocInstaller", quietly = T)
-biocLite("RforProteomics", dependencies = TRUE)
-#library RforProteomics (has the libraries needed)
-library(RforProteomics)
+
+# RforProteomics contains the libraries needed
+if(!require("RforProteomics", quietly = T)){
+  biocLite("RforProteomics", dependencies = TRUE)
+  require("RforProteomics", quietly = T)
+}
 
 # install if needed and load "shiny" lib
 if(!require("shiny", quietly = T))  
@@ -18,4 +24,5 @@ require("shiny", quietly = T)
 if(!require("lattice", quietly = T))  
   install.packages("lattice")
 require("lattice", quietly = T)
+
 
